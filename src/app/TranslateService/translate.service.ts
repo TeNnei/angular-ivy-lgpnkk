c52b5649ecde6b4db1e3678aeaa7c3c06a8337c6
TeNnei <70272751+TeNnei@users.noreply.github.com>
2022-12-12 10:05:01 +0500

    1 file modified

---
+++ src/app/TranslateService/translate.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+import {TranslateObject} from './translate.object.ts';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable()
+export class TranslateService {
+
+}
