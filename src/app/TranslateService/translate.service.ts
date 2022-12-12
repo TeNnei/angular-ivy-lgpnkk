dfa0a5550aeb4c340f82f51c0588c0e3ff285570
TeNnei <70272751+TeNnei@users.noreply.github.com>
2022-12-12 10:25:05 +0500

    1 file modified

--- src/app/TranslateService/translate.service.ts
+++ src/app/TranslateService/translate.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
-import {TranslateObject} from './translate.object.ts';
-import {HttpClient} from '@angular/common/http';
+import {TranslateObject} from './translate.object';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable()
 export class TranslateService {
 
+  constructor(private http: HttpClient){ }
+
+  translate(translateObject: TranslateObject){
+
+    const headers = new HttpHeaders()
+    .set('Content-Type', 'application/json; charset=utf-8')
+    .set('X-goog-api-key', 'AIzaSyDyfFMvi8tGXscwDpGHd40p3Fa7Gk5s7N0');
+
+    const body = {
+      q: translateObject.q, 
+      source: translateObject.source, 
+      target: translateObject.target, 
+      format: translateObject.format}
+
+     return this.http.post('https://translation.googleapis.com/language/translate/v2', body, {headers: headers})
+  }
+  
 }
